3b481bbe82ff1aa62fbe8c40bf87beabb34e4eb5
Mickael <mickael.mesloub@gmail.com>
2025-01-27 18:07:43 +0100

    refactor(app component): replace default app component content with our app-playing-card component and remove test file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
+  imports: [PlayingCardComponent],
 })
-export class AppComponent {
-  title = 'angular-18-tuto';
-}
+export class AppComponent {}
